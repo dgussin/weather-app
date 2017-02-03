e1b565f76e22281293282499d5eacf923fd0ccbc
Darren Gussin <dgussin@msn.com>
2017-02-02 21:43:12 -0500

    Display the formatted address, latitude and longitude

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=31145%20N%20Park%20Dr%20Farmington%20Hills',
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=31145%20N%20Park%20Dr%20Farmington%20Hills',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
 });
